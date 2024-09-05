bd1867c7713927bf55a2ff4f9287b1c36c620517
alohasally <peacheeks@gmail.com>
2024-09-05 22:43:04 +0900

    upload video function

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const privateOptions = [
+  { value: 0, label: "public" },
+  { value: 1, label: "private" },
+];
+
+const categoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+  { value: 4, label: "Sports" },
+  { value: 5, label: "Travel & Events" },
+];
 
 function VideoUploadPage() {
-  const { Title } = Typography;
-  const { TextArea } = Input;
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoDescription, setVideoDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("film & Animation");
+
+  const onTitleChange = (e) => {
+    setVideoTitle(e.target.value);
+  };
+
+  const onDescriptChange = (e) => {
+    setVideoDescription(e.target.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.target.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("Failed to upload video");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -12,7 +65,7 @@ function VideoUploadPage() {
       </div>
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multipl={false} maxSize={100000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
@@ -38,25 +91,33 @@ function VideoUploadPage() {
         <br />
         <br />
         <label>Title</label>
-        <Input onChange="" value="" />
+        <Input onChange={onTitleChange} value={videoTitle} />
         <br />
         <br />
         <label>Description</label>
-        <TextArea onChange="" value="" />
+        <TextArea onChange={onDescriptChange} value={videoDescription} />
         <br />
         <br />
 
-        <select onChange="" value="">
-          <option key value="">
-            Select Category
-          </option>
+        <select onChange={onPrivateChange} value={Private}>
+          {privateOptions.map((opt, index) => {
+            return (
+              <option key={index} value={opt.value}>
+                {opt.label}
+              </option>
+            );
+          })}
         </select>
         <br />
         <br />
-        <select onChange="" value="">
-          <option key value="">
-            Select Category
-          </option>
+        <select onChange={onCategoryChange} value={Category}>
+          {categoryOptions.map((category, index) => {
+            return (
+              <option key={index} value={category.value}>
+                {category.label}
+              </option>
+            );
+          })}
         </select>
         <br />
         <br />
---
+++ server/routes/video.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+// const { Video } = require("../models/User");
+
+const { auth } = require("../middleware/auth");
+const multer = require("multer");
+
+// STORAGE MULTER CONFIG
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== ".mp4" && ext !== ".avi" && ext !== ".mov") {
+      return cb(res.status(400).end("Only mp4, avi, mov is allowed"), false);
+    }
+    cb(null, true);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+//=================================
+//             Video
+//=================================
+
+router.post("/uploadfiles", (req, res) => {
+  //save video to the server
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      url: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+module.exports = router;
